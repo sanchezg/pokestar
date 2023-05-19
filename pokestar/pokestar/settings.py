358b40c67ae6e238a6aaea602c31d4e27e7b9c3a
Gonzalo Sanchez <gonchi.sanchez@gmail.com>
2023-05-19 17:00:32 -0300

    Config management. Wire Postgres

--- pokestar/pokestar/settings.py
+++ pokestar/pokestar/settings.py
@@ -10,11 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = os.environ.get("ENV", "LOCAL").lower()
+
+dotenv_path = f"{BASE_DIR}/config/.env.{env}"
+
+load_dotenv(dotenv_path=dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -79,8 +87,12 @@ WSGI_APPLICATION = "pokestar.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_USER"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_HOST", "5432"),
     }
 }
 
